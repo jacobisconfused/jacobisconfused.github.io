da4f13bcbc02dd932fb09daafe0af5996c75f4bf
jacobisconfused <deadnoggin@gmail.com>
2025-01-10 18:10:11 -0500

    Add files via upload

---
+++ js/4485.c3dc2c95.js
@@ -0,0 +1,9 @@
+"use strict";(self["webpackChunkrando_commando"]=self["webpackChunkrando_commando"]||[]).push([[4485],{94485:function(e,t,n){n.r(t),n.d(t,{conf:function(){return o},language:function(){return s}});
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+var o={comments:{lineComment:"#"}},s={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}}}}]);
+//# sourceMappingURL=4485.c3dc2c95.js.map
